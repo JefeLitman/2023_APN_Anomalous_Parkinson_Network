3522090155f5207d4aad1545068d7de217bcc830
Edgar RP <jefelitman@gmail.com>
2021-05-13 06:30:00 +0000

    Ganomaly preprocessing v1.1

--- models/ganomaly/utils/preprocessing.py
+++ models/ganomaly/utils/preprocessing.py
@@ -1,6 +1,6 @@
 """This file contains the different methods to preprocess the data for GANomaly
 in Tensorflow.
-Version: 1.0
+Version: 1.1
 Made by: Edgar Rangel
 """
 
@@ -10,10 +10,10 @@ def normalize_accros_channels(data, label, mean, std):
     """Function that normalize the tensors in the last axis (channels) with mean and standard deviation 
     given.
     Args:
-        data: A Tensor with to be normalized in the last axis (Tensor Instance).
-        label: A Tensor that contains the true label for the dataset (Tensor Instance).
-        mean: An integer specifying the mean to normalize the data (Integer).
-        std: An integer specifying the standard deviation to normalize the data (Integer).
+        data (Tensor): A Tensor with to be normalized in the last axis.
+        label (Tensor): A Tensor that contains the true label for the dataset.
+        mean (Decimal): An integer specifying the mean to normalize the data.
+        std (Decimal): An integer specifying the standard deviation to normalize the data.
     """
     normalized = data - mean / std
     return normalized, label
@@ -22,10 +22,10 @@ def min_max_scaler(data, label, new_min, new_max):
     """Function that scale tensors using min max scaler formula letting the values between the new min and 
     max values given.
     Args:
-        data: A Tensor with to be normalized in the last axis (Tensor Instance).
-        label: A Tensor that contains the true label for the dataset (Tensor Instance).
-        new_min: An integer specifying the new minimum value for the data (Integer).
-        new_max: An integer specifying the new maximum value for the data (Integer).
+        data (Tensor): A Tensor with to be scaled in the last axis.
+        label (Tensor): A Tensor that contains the true label for the dataset.
+        new_min (Decimal): An integer specifying the new minimum value for the data.
+        new_max (Decimal): An integer specifying the new maximum value for the data.
     """
     data_std = (data - tf.reduce_min(data)) / (tf.reduce_max(data) - tf.reduce_min(data))
     data_scaled = data_std * (new_max - new_min) + new_min
@@ -34,18 +34,18 @@ def min_max_scaler(data, label, new_min, new_max):
 def resize(data, label, size):
     """Function that resize a 3D tensor frame by frame or 2D tensor given a specified size.
     Args:
-        data: A Tensor with 3 or 2 dimension to be resized (Tensor Instance).
-        label: A Tensor that contains the true label for the dataset (Tensor Instance).
-        size: A Tuple with 2 elements (height, width) respectively being the new size of the frames (Tuple).
+        data (Tensor): A Tensor with 3 or 2 dimension to be resized.
+        label (Tensor): A Tensor that contains the true label for the dataset.
+        size (Tuple): A Tuple with 2 elements (height, width) respectively being the new size of the frames.
     """
     return tf.image.resize(data, size), label
 
 def get_center_of_volume(data, label, n_frames):
     """Function that get the center portion of 3D tensor (video) given the n frames.
     Args:
-        data: A Tensor with 3 or 2 dimension to be resized (Tensor Instance).
-        label: A Tensor that contains the true label for the dataset (Tensor Instance).
-        n_frames: An integer specifying the quantity of frames to extract from data (Integer).
+        data (Tensor): A Tensor with 3 or 2 dimension to be resized.
+        label (Tensor): A Tensor that contains the true label for the dataset.
+        n_frames (Integer): An integer specifying the quantity of frames to extract from data.
     """
     half = tf.math.floordiv(tf.shape(data)[0], 2)
     return data[half - n_frames//2 : half + n_frames//2], label
@@ -53,7 +53,16 @@ def get_center_of_volume(data, label, n_frames):
 def undo_enumerate(i, xy):
     """Function that revert the enumerate method applied over a tf.data.Dataset.
     Args:
-        i: An integer with the index of the data in the Dataset instance (Integer).
-        xy: A Tuple of Tensor instances that contains the data and true label respectively (Tuple).
+        i (Integer): An integer with the index of the data in the Dataset instance.
+        xy (Tuple): A Tuple of Tensor instances that contains the data and true label respectively.
     """
     return xy[0], xy[1]
+
+def delete_patient_id(data, label, patient_id):
+    """Function to delete the patient id variable return in a tf.data.Dataset.
+    Args:
+        data (Tensor): A Tensor instance that contains the video data.
+        label (Tensor): A Tensor that contains the true label for the dataset.
+        patient_id (Tensor): A Tensor that contains the patient id of the data.
+    """
+    return data, label
