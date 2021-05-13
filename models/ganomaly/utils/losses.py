9ee4b57ff50a0fb3d8471af81301b7e63af11ca0
Edgar RP <jefelitman@gmail.com>
2021-05-13 06:29:40 +0000

    Ganomaly loss v1.1

--- models/ganomaly/utils/losses.py
+++ models/ganomaly/utils/losses.py
@@ -1,6 +1,6 @@
 """This file contains the losses for GANomaly nets translated in Tensorflow.
 https://arxiv.org/abs/1805.06725
-Version: 1.0
+Version: 1.1
 Made by: Edgar Rangel
 """
 
@@ -13,23 +13,41 @@ BCE = tf.keras.losses.BinaryCrossentropy()
 def l1_loss(y_true, y_pred):
     """Function that return a function callback of the l1_loss used in GANomaly model.
     Args:
-        y_true: A Tensor with the real data of the dataset (Tensor Instance).
-        y_pred: A Tensor being the output of the model (Tensor Instance).
+        y_true (Tensor): A Tensor with the real data of the dataset.
+        y_pred (Tensor): A Tensor being the output of the model.
     """
     return MAE(y_true, y_pred)
 
 def l2_loss(y_true, y_pred):
     """Function that return a function callback of the l2_loss used in GANomaly model.
     Args:
-        y_true: A Tensor with the real data of the dataset (Tensor Instance).
-        y_pred: A Tensor being the output of the model (Tensor Instance).
+        y_true (Tensor): A Tensor with the real data of the dataset.
+        y_pred (Tensor): A Tensor being the output of the model.
     """
     return MSE(y_true, y_pred)
 
 def BCELoss(y_true, y_pred):
     """Function that return a function callback of the bce_loss used in GANomaly model.
     Args:
-        y_true: A Tensor with the real data of the dataset (Tensor Instance).
-        y_pred: A Tensor being the output of the model (Tensor Instance).
+        y_true (Tensor): A Tensor with the real data of the dataset.
+        y_pred (Tensor): A Tensor being the output of the model.
     """
     return BCE(y_true, y_pred)
+
+def l1_loss_batch(y_true, y_pred):
+    """Function that return the l1_loss sample by sample (no batch dimension reduction)
+    used in GANomaly model.
+    Args:
+        y_true (Tensor): A Tensor with the real data of the dataset.
+        y_pred (Tensor): A Tensor being the output of the model.
+    """
+    return tf.reduce_mean(tf.math.abs(y_true-y_pred), axis=range(1, tf.rank(y_true).numpy()))
+
+def l2_loss_batch(y_true, y_pred):
+    """Function that return the l2_loss sample by sample (no batch dimension reduction)
+    used in GANomaly model.
+    Args:
+        y_true (Tensor): A Tensor with the real data of the dataset.
+        y_pred (Tensor): A Tensor being the output of the model.
+    """
+    return tf.reduce_mean(tf.math.pow(y_true-y_pred, 2), axis=range(1, tf.rank(y_true).numpy()))
