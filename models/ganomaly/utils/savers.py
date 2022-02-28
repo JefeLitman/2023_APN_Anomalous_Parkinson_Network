25be9a77ac581c5d28fc50dd90a15d80e68946f7
Edgar RP <jefelitman@gmail.com>
2022-02-28 14:28:40 +0000

    Models Ganomaly Utils Savers v1.1

--- models/ganomaly/utils/savers.py
+++ models/ganomaly/utils/savers.py
@@ -1,9 +1,10 @@
 """This file contains methods to save the errors for GANomaly models.
-Version: 1.0.2
+Version: 1.1
 Made by: Edgar Rangel
 """
 
 import os
+import cv2
 import numpy as np
 
 def __make_subfolders__(folder_path, training):
@@ -25,6 +26,20 @@ def __make_subfolders__(folder_path, training):
         os.mkdir(root_path)
 
     return normal_path, abnormal_path
+
+def __format_number__(index):
+    """This function format the index integer into a string in the range of 0000 to 9999.
+    Args:
+        index (Int): Integer to be formatted.
+    """
+    if index + 1 < 10:
+        return '000' + str(index + 1)
+    elif index + 1 < 100:
+        return '00' + str(index + 1)
+    elif index + 1 < 1000:
+        return '0' + str(index + 1)
+    else:
+        return str(index + 1)
 
 def save_errors(batch_errors, batch_labels, folder_path, training):
     """Function to save the batch of errors in the given folder path for train or test data, 
@@ -62,3 +77,55 @@ def save_errors(batch_errors, batch_labels, folder_path, training):
 
     if abnormal_errors.shape[0] != 0:
         np.save(abnormal_path, abnormal_errors)
+
+def save_frames(batch_frames, batch_labels, batch_ids, batch_video_id, batch_frame_id, folder_path, training):
+    """Function to save the batch of frames in the given folder path for train or test data, 
+    subdividing the normal and abnormal samples on different folders and making a folder per 
+    patient and saving its frames by order.
+    Args:
+        batch_frames (Array): A 4D array with (b, h, w, c) shapes respectively 
+        where b - batch size, f - frames, h - height, w - width and 
+        c - channels with the videos to be saved.
+        batch_labels (Array): A 1D array with (b) shape with the labels of videos.
+        batch_ids (Array): A 1D array with (b) shape with the patient id of every video.
+        batch_video_id (Array): A 1D array with (b) shape indicating what video of the patient is saved.
+        batch_frame_id (Array): A 1D array with (b) shape indicating what frame of the video is.
+        folder_path (String): The root path where the videos will be saved.
+        model_dimension (String): Dimensionality on which the model's convolutions operate, can be "3D" or "2D".
+        training (Boolean): Select True if the videos comes from the train data or False otherwise.
+    """
+    assert batch_labels.shape[0] == batch_frames.shape[0]
+            
+    normal_path, abnormal_path = __make_subfolders__(folder_path, training)
+    if not os.path.isdir(normal_path):
+        os.mkdir(normal_path)
+    if not os.path.isdir(abnormal_path):
+        os.mkdir(abnormal_path)
+
+    for i, frame in enumerate(batch_frames):
+        if batch_labels[i] == 0:
+            save_path =  normal_path
+        elif batch_labels[i] == 1:
+            save_path = abnormal_path
+        else:
+            raise AssertionError('There is an unknow label in the data. Label found {}, expected to be 0 or 1'.format(batch_labels[i]))
+
+        folder = "patient-{}_video-{}".format(
+            __format_number__(batch_ids[i]),
+            __format_number__(batch_video_id[i])
+        )
+        save_path = os.path.join(save_path, folder)
+        if not os.path.isdir(save_path):
+            os.mkdir(save_path)
+        
+        assert frame.ndim == 3
+        if frame.shape[-1] == 1:
+            convert = None
+        elif frame.shape[-1] == 3:
+            convert = cv2.COLOR_RGB2BGR
+        else:
+            raise AssertionError("The video is not in RGB or gray scale to be saved.")
+
+        frame_converted = cv2.cvtColor(frame, convert)
+        filename = __format_number__(batch_frame_id) + ".png"
+        cv2.imwrite(os.path.join(save_path, filename), frame_converted)
