5e97271c965f69b7676ac6d0143b5c43612946e2
Edgar RP <jefelitman@gmail.com>
2022-05-03 17:51:32 -0500

    Scripts Ganomaly Base model v0.1

---
+++ scripts/ganomaly/base_model.py
@@ -0,0 +1,4 @@
+"""This file contain the running script for training and inference the base model of Ganomaly 2D. This file contain specifically the preprocessing applied
+Version: 0.1
+Made by: Edgar Rangel
+"""
