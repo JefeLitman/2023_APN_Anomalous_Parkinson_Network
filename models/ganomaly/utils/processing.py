15798e9101f0be81581fa261215cb33c8d9d3c4a
Edgar RP <jefelitman@gmail.com>
2022-06-16 16:03:46 -0500

    Models Ganomaly Utils Processing v1.5

--- models/ganomaly/utils/processing.py
+++ models/ganomaly/utils/processing.py
@@ -1,14 +1,12 @@
-"""This file contains the different methods to process the data for GANomaly
-in Tensorflow.
-Version: 1.4.1
+"""This file contains the different methods to process the data for GANomaly in Tensorflow.
+Version: 1.5
 Made by: Edgar Rangel
 """
 
 import tensorflow as tf 
 
 def normalize_accros_channels(data, label, mean, std, patient_id):
-    """Function that normalize the tensors in the last axis (channels) with mean and standard deviation 
-    given.
+    """Function that normalize the tensors in the last axis (channels) with mean and standard deviation given.
     Args:
         data (Tensor): A Tensor with to be normalized in the last axis.
         label (Tensor): A Tensor that contains the true label for the dataset.
@@ -20,8 +18,7 @@ def normalize_accros_channels(data, label, mean, std, patient_id):
     return normalized, label, patient_id
 
 def min_max_scaler(data, label, new_min, new_max, patient_id):
-    """Function that scale tensors using min max scaler formula letting the values between the new min and 
-    max values given.
+    """Function that scale tensors using min max scaler formula letting the values between the new min and max values given.
     Args:
         data (Tensor): A Tensor with to be scaled in the last axis.
         label (Tensor): A Tensor that contains the true label for the dataset.
@@ -53,6 +50,49 @@ def get_center_of_volume(data, label, n_frames, patient_id):
     """
     half = tf.math.floordiv(tf.shape(data)[0], 2)
     return data[half - n_frames//2 : half + n_frames//2], label, patient_id
+
+def oversampling_equidistant_full(data, label, n_frames, patient_id):
+    """Function that make always an oversampling of the data (video) with at least one extended sample or multiple samples extended if the data have more frames than the required.
+    Args:
+        data (Tensor): A Tensor with 4 dimension to be cropped with (frames, height, width, channels) shape.
+        label (Tensor): A Tensor that contains the true label for the dataset.
+        n_frames (Integer): An integer specifying the quantity of frames to extract from data.
+        patient_id (Tensor): A Tensor that contains the patient id of the data.
+    """
+    # Calculating basic parameters
+    x = data
+    x_frames = tf.shape(x)[0]
+    q = tf.cast(tf.math.ceil(x_frames / n_frames), tf.int32)
+    k = tf.abs(q * n_frames - x_frames)
+    
+    #Condition to repeat the center if its possible
+    if k <= x_frames:
+        half_x = tf.cast(tf.math.floor(x_frames / 2), tf.int32)
+        half_k = tf.cast(tf.math.floor(k / 2), tf.int32)
+        if k % 2 == 0:
+            augmented_volume = tf.repeat(x[half_x - half_k : half_x + half_k], [2], axis=0)
+            total_volume = tf.concat([x[:half_x - half_k], augmented_volume, x[half_x + half_k:]], axis=0)
+        else:
+            augmented_volume = tf.repeat(x[half_x - half_k : half_x + half_k + 1], [2], axis=0)
+            total_volume = tf.concat([x[:half_x - half_k], augmented_volume, x[half_x + half_k + 1:]], axis=0)
+    else:
+        k = tf.cast(tf.math.ceil(n_frames / x_frames), tf.int32)
+        augmented_volume = tf.repeat(x, [k], axis=0)
+        half_x = tf.cast(tf.math.floor((k * x_frames) / 2), tf.int32)
+        half_k = tf.cast(tf.math.floor(n_frames / 2), tf.int32)
+        total_volume = augmented_volume[half_x - half_k : half_x + half_k]
+        
+    volumes = total_volume[0::q]
+    for i in tf.range(1, q):
+        volumes = tf.concat([volumes, total_volume[i::q]], axis=0)
+    samples = tf.reshape(volumes, tf.concat([[q, n_frames], tf.shape(x)[1:]], axis=0))
+    
+    labels = tf.tile([label], [q]) # Repeat the label 
+    ids = tf.tile([patient_id], [q]) # Repeat the patient_id 
+    xs = tf.data.Dataset.from_tensor_slices(samples)
+    ys = tf.data.Dataset.from_tensor_slices(labels)
+    zs = tf.data.Dataset.from_tensor_slices(ids)
+    return tf.data.Dataset.zip((xs, ys, zs))
 
 def undo_enumerate(i, xyi):
     """Function that revert the enumerate method applied over a tf.data.Dataset.
