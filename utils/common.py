851fc171b5535d484dd7b5de359dac0920751a11
Edgar RP <edgar.rangel@correo.uis.edu.co>
2022-06-26 00:37:12 +0000

    Utils Common v1.0

---
+++ utils/common.py
@@ -0,0 +1,47 @@
+"""This file contains common functions used across different metrics to use with the models.
+Version: 1.0
+Made by: Edgar Rangel
+"""
+
+import os
+
+def format_index(index, max_digits = 4):
+    """This function format the index integer into a string with the maximun quantity of digits given.
+    Args:
+        index (Int): Integer to be formatted.
+        max_digits (Int): How many digits must the number contain, e.g: if 4 then the range is from 0000 to 9999.
+    """
+    value = str(index)
+    while len(value) < max_digits:
+        value = '0' + value
+    return value
+
+def get_train_test_paths(base_path, training):
+    """Function that returns two paths for normal or abnormal data that will be saved. It take into account if the data will be saved for train or test data and create the respective folder. The first returned folder is for normal and last for abnormal.
+    Args:
+        base_path (String): The root path where the data will be saved.
+        training (Boolean): Select True if the videos comes from the train data or False otherwise.
+    """
+    if training:
+        root_path = os.path.join(base_path, "train")
+    else:
+        root_path = os.path.join(base_path, "test")
+
+    normal_path = os.path.join(root_path, "normal")
+    abnormal_path = os.path.join(root_path, "abnormal")
+        
+    if not os.path.isdir(root_path):
+        os.mkdir(root_path)
+
+    return normal_path, abnormal_path
+
+def get_next_last_item(base_path):
+    """Function than will return an integer with an id of the following element for the last item listed in the base path. Also, this method checks it the elements in the folder are formatted with the following order (xxxx_*) where xxxx is a sequences of integers.
+    Args:
+        base_path (String): The root path in where the elements will be listed.
+    """
+    elements = [int(i.split("_")[0]) for i in sorted(os.listdir(base_path))]
+    if len(elements) == 0:
+        return 1
+    else:
+        return elements[-1] + 1
