a1ce48b4347b43123c85cbcb04d6104721d8265d
Edgar RP <jefelitman@gmail.com>
2022-07-28 11:04:39 -0500

    Model Ganomaly Utils Losses v1.2

--- models/ganomaly/utils/losses.py
+++ models/ganomaly/utils/losses.py
@@ -1,6 +1,6 @@
-"""This file contains the losses for GANomaly nets translated in Tensorflow.
+"""This file contains the losses for GANomaly model.
 https://arxiv.org/abs/1805.06725
-Version: 1.1
+Version: 1.2
 Made by: Edgar Rangel
 """
 
@@ -11,7 +11,7 @@ MSE = tf.keras.losses.MeanSquaredError()
 BCE = tf.keras.losses.BinaryCrossentropy()
 
 def l1_loss(y_true, y_pred):
-    """Function that return a function callback of the l1_loss used in GANomaly model.
+    """Function that return a function callback of the l1_loss used in GANomaly base model.
     Args:
         y_true (Tensor): A Tensor with the real data of the dataset.
         y_pred (Tensor): A Tensor being the output of the model.
@@ -19,7 +19,7 @@ def l1_loss(y_true, y_pred):
     return MAE(y_true, y_pred)
 
 def l2_loss(y_true, y_pred):
-    """Function that return a function callback of the l2_loss used in GANomaly model.
+    """Function that return a function callback of the l2_loss used in GANomaly base model.
     Args:
         y_true (Tensor): A Tensor with the real data of the dataset.
         y_pred (Tensor): A Tensor being the output of the model.
@@ -27,7 +27,7 @@ def l2_loss(y_true, y_pred):
     return MSE(y_true, y_pred)
 
 def BCELoss(y_true, y_pred):
-    """Function that return a function callback of the bce_loss used in GANomaly model.
+    """Function that return a function callback of the bce_loss used in GANomaly base model.
     Args:
         y_true (Tensor): A Tensor with the real data of the dataset.
         y_pred (Tensor): A Tensor being the output of the model.
@@ -35,8 +35,7 @@ def BCELoss(y_true, y_pred):
     return BCE(y_true, y_pred)
 
 def l1_loss_batch(y_true, y_pred):
-    """Function that return the l1_loss sample by sample (no batch dimension reduction)
-    used in GANomaly model.
+    """Function that return the l1_loss sample by sample (no batch dimension reduction) used in GANomaly base model.
     Args:
         y_true (Tensor): A Tensor with the real data of the dataset.
         y_pred (Tensor): A Tensor being the output of the model.
@@ -44,8 +43,7 @@ def l1_loss_batch(y_true, y_pred):
     return tf.reduce_mean(tf.math.abs(y_true-y_pred), axis=range(1, tf.rank(y_true).numpy()))
 
 def l2_loss_batch(y_true, y_pred):
-    """Function that return the l2_loss sample by sample (no batch dimension reduction)
-    used in GANomaly model.
+    """Function that return the l2_loss sample by sample (no batch dimension reduction) used in GANomaly base model.
     Args:
         y_true (Tensor): A Tensor with the real data of the dataset.
         y_pred (Tensor): A Tensor being the output of the model.
