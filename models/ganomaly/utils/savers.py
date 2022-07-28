b9a8317f8844a46fd9513ba8d1f0d210a80043fd
Edgar RP <jefelitman@gmail.com>
2022-07-28 11:10:46 -0500

    Model Ganomaly Utils Savers v1.2

--- models/ganomaly/utils/savers.py
+++ models/ganomaly/utils/savers.py
@@ -1,102 +1,33 @@
-"""This file contains methods to save the errors for GANomaly models.
-Version: 1.1.2
+"""This file contains methods to save elements for GANomaly model.
+https://arxiv.org/abs/1805.06725
+Version: 1.2
 Made by: Edgar Rangel
 """
 
 import os
-import cv2
 import numpy as np
+import tensorflow as tf
+from .processing import min_max_scaler
+from .losses import l1_loss_batch, l2_loss_batch
+from utils.savers import save_frame, save_latent_vector, save_errors
+from utils.common import format_index, get_train_test_paths, get_next_last_item
 
-def __make_subfolders__(folder_path, training):
-    """Function that create the respectively subfolders to save the train/test normal 
-    and abnormal videos or vectors and returns the normal and abnormal folder paths.
+def save_frames(batch_frames, batch_labels, batch_ids, batch_frames_ids, batch_videos_ids, folder_path, training):
+    """Function to save the batch of frames in the given folder path for train or test data, subdividing the normal and abnormal samples on different folders.
     Args:
+        batch_frames (Array): A 4D array with (b, h, w, c) shape where b - batch size, h - height, w - width and c - channels of the videos to be saved.
+        batch_labels (Array): A 1D array with (b) shape with the labels of videos.
+        batch_ids (Array): A 1D array with (b) shape with the patients ids of every video.
+        batch_frames_ids (Array): A 1D array with (b) shape with the ids of each frame in the batch.
+        batch_videos_ids (Array): A 1D array with (b) shape with the ids of videos in the batch.
         folder_path (String): The root path where the videos will be saved.
         training (Boolean): Select True if the videos comes from the train data or False otherwise.
     """
-    if training:
-        root_path = os.path.join(folder_path, "train")
-    else:
-        root_path = os.path.join(folder_path, "test")
-
-    normal_path = os.path.join(root_path, "normal")
-    abnormal_path = os.path.join(root_path, "abnormal")
-        
-    if not os.path.isdir(root_path):
-        os.mkdir(root_path)
-
-    return normal_path, abnormal_path
-
-def __format_number__(index):
-    """This function format the index integer into a string in the range of 0000 to 9999.
-    Args:
-        index (Int): Integer to be formatted.
-    """
-    if index < 10:
-        return '000' + str(index)
-    elif index < 100:
-        return '00' + str(index)
-    elif index < 1000:
-        return '0' + str(index)
-    else:
-        return str(index)
-
-def save_errors(batch_errors, batch_labels, folder_path, training):
-    """Function to save the batch of errors in the given folder path for train or test data, 
-    subdividing the normal and abnormal samples on different files.
-    Args:
-        batch_errors (Array): A 1D array with (b) shape with the errors to be saved.
-        batch_labels (Array): A 1D array with (b) shape with the labels of videos.
-        folder_path (String): The root path where the errors will be saved.
-        training (Boolean): Select True if the videos comes from the train data or False otherwise.
-    """
-    assert batch_labels.shape[0] == batch_errors.shape[0]
-
-    normal_path, abnormal_path = __make_subfolders__(folder_path, training)
-
-    if os.path.isfile(normal_path+".npy"):
-        normal_errors = np.load(normal_path+".npy")
-    else:
-        normal_errors = np.r_[[]]
-
-    if os.path.isfile(abnormal_path+".npy"):
-        abnormal_errors = np.load(abnormal_path+".npy")
-    else:
-        abnormal_errors = np.r_[[]]
-
-    for i, label in enumerate(batch_labels):
-        if label == 0:
-            normal_errors = np.concatenate([normal_errors, [batch_errors[i]]])
-        elif label == 1:
-            abnormal_errors = np.concatenate([abnormal_errors, [batch_errors[i]]])
-        else:
-            raise AssertionError('There is an unknow label in the data. Label found {}, expected to be 0 or 1'.format(batch_labels[i]))
-
-    if normal_errors.shape[0] != 0:
-        np.save(normal_path, normal_errors)
-
-    if abnormal_errors.shape[0] != 0:
-        np.save(abnormal_path, abnormal_errors)
-
-def save_frames(batch_frames, batch_labels, batch_ids, batch_video_id, batch_frame_id, folder_path, training):
-    """Function to save the batch of frames in the given folder path for train or test data, 
-    subdividing the normal and abnormal samples on different folders and making a folder per 
-    patient and saving its frames by order.
-    Args:
-        batch_frames (Array): A 4D array with (b, h, w, c) shapes respectively 
-        where b - batch size, f - frames, h - height, w - width and 
-        c - channels with the videos to be saved.
-        batch_labels (Array): A 1D array with (b) shape with the labels of videos.
-        batch_ids (Array): A 1D array with (b) shape with the patient id of every video.
-        batch_video_id (Array): A 1D array with (b) shape indicating what video of the patient is saved.
-        batch_frame_id (Array): A 1D array with (b) shape indicating what frame of the video is.
-        folder_path (String): The root path where the videos will be saved.
-        model_dimension (String): Dimensionality on which the model's convolutions operate, can be "3D" or "2D".
-        training (Boolean): Select True if the videos comes from the train data or False otherwise.
-    """
-    assert batch_labels.shape[0] == batch_frames.shape[0]
+    assert batch_labels.shape[0] == batch_frames.shape[0] == batch_ids.shape[0] == batch_videos_ids.shape[0] == batch_frames_ids.shape[0]
+    if batch_frames.ndim != 4:
+        raise AssertionError('The quantity of dimension for batch_frames must be 4 in for GANomaly model')
             
-    normal_path, abnormal_path = __make_subfolders__(folder_path, training)
+    normal_path, abnormal_path = get_train_test_paths(folder_path, training)
     if not os.path.isdir(normal_path):
         os.mkdir(normal_path)
     if not os.path.isdir(abnormal_path):
@@ -109,26 +40,124 @@ def save_frames(batch_frames, batch_labels, batch_ids, batch_video_id, batch_fra
             save_path = abnormal_path
         else:
             raise AssertionError('There is an unknow label in the data. Label found {}, expected to be 0 or 1'.format(batch_labels[i]))
-
-        folder = "patient-{}_video-{}".format(
-            __format_number__(batch_ids[i]),
-            __format_number__(batch_video_id[i])
+        
+        index = get_next_last_item(save_path)
+        for folder in sorted(os.listdir(save_path)):
+            if folder.endswith("patient-{}_video-{}".format(
+                batch_ids[i], 
+                batch_videos_ids[i]
+            )):
+                index = int(folder.split('_')[0])
+                
+        folder = "{}_patient-{}_video-{}".format(
+            format_index(index),
+            format_index(batch_ids[i]),
+            format_index(batch_videos_ids[i])
         )
         save_path = os.path.join(save_path, folder)
         if not os.path.isdir(save_path):
             os.mkdir(save_path)
-        
-        assert frame.ndim == 3
-        if frame.shape[-1] == 1:
-            convert = None
-        elif frame.shape[-1] == 3:
-            convert = cv2.COLOR_RGB2BGR
-        else:
-            raise AssertionError("The video is not in RGB or gray scale to be saved.")
+        save_frame(frame, batch_frames_ids[i], save_path)
 
-        if convert:
-            frame_converted = cv2.cvtColor(frame, convert)
+def save_latent_vectors(batch_latents, batch_labels, batch_ids, batch_frames_ids, batch_videos_ids, folder_path, training):
+    """Function to save the batch of embeddings in the given folder path for train or test data, subdividing the normal and abnormal samples on different folders.
+    Args:
+        batch_latents (Array): A 2D numpy array with shape (b, z) where b = batch size and z = context vector size for the latent_vectors to be saved.
+        batch_labels (Array): A 1D array with shape (b) for the labels of videos.
+        batch_ids (Array): A 1D array with shape (b) for the patients ids of every video.
+        batch_frames_ids (Array): A 1D array with (b) shape with the ids of each frame in the batch.
+        batch_videos_ids (Array): A 1D array with shape (b) for the ids of videos in the batch.
+        folder_path (String): The root path where the videos will be saved.
+        training (Boolean): Select True if the videos comes from the train data or False otherwise.
+    """
+    assert batch_labels.shape[0] == batch_latents.shape[0] == batch_ids.shape[0] == batch_frames_ids.shape[0] == batch_videos_ids.shape[0]
+    if batch_latents.ndim != 2:
+        raise AssertionError('The quantity of dimension for batch_latents must be 2 in for GANomaly model')
+            
+    normal_path, abnormal_path = get_train_test_paths(folder_path, training)
+    if not os.path.isdir(normal_path):
+        os.mkdir(normal_path)
+    if not os.path.isdir(abnormal_path):
+        os.mkdir(abnormal_path)
+
+    for i, vector in enumerate(batch_latents):
+        if batch_labels[i] == 0:
+            save_path =  normal_path
+        elif batch_labels[i] == 1:
+            save_path = abnormal_path
         else:
-            frame_converted = frame
-        filename = __format_number__(batch_frame_id[i]) + ".png"
-        cv2.imwrite(os.path.join(save_path, filename), frame_converted)
+            raise AssertionError('There is an unknow label in the data. Label found {}, expected to be 0 or 1'.format(batch_labels[i]))
+
+        index = get_next_last_item(save_path)
+        for folder in sorted(os.listdir(save_path)):
+            if folder.endswith("patient-{}_video-{}".format(
+                batch_ids[i], 
+                batch_videos_ids[i]
+            )):
+                index = int(folder.split('_')[0])
+
+        folder_embeddings = "{}_patient-{}_video-{}".format(
+            format_index(index),
+            format_index(batch_ids[i]),
+            format_index(batch_videos_ids[i])
+        )
+        save_path = os.path.join(save_path, folder_embeddings)
+        if not os.path.isdir(save_path):
+            os.mkdir(save_path)
+        save_latent_vector(vector, save_path, 'frame_{}'.format(format_index(batch_frames_ids[i])))
+
+
+def save_models(gen_model, disc_model, experiment_path, epoch = ""):
+    """This function take the GANomaly models and save them in the path given. Also it remove the previous models in the path to avoid saving a lot of innecessary files.
+    Args:
+        gen_model (Keras Model Instance): An instance of tf.keras.Model of the generator model to be saved.
+        disc_model (Keras Model Instance): An instance of tf.keras.Model of the discriminator model to be saved.
+        experiment_path (String): A string with the folder path in where the models will be saved.
+        epoch (Integer): An optional integer indicating in which epoch the model reach while is training.
+    """
+    for i in sorted(os.listdir(experiment_path)):
+        if "gen_model" in i:
+            os.remove(os.path.join(experiment_path, i))
+        elif "disc_model" in i:
+            os.remove(os.path.join(experiment_path, i))
+    if epoch != "":
+        to_add = "_{}".format(epoch + 1)
+    else:
+        to_add = ""
+    gen_model.save(os.path.join(experiment_path,"gen_model{}.h5".format(to_add)), 
+        include_optimizer=False, save_format='h5')
+    disc_model.save(os.path.join(experiment_path,"disc_model{}.h5".format(to_add)), 
+        include_optimizer=False, save_format='h5')
+
+def save_model_results(xyi, fake_images, latent_i, latent_o, feat_real, feat_fake, outputs_path, training, clean_old):
+    """This function take the given args (xyi, fake_images, latent_i, latent_o, feat_real, feat_fake) and save them in the outputs paths given for their own reason. This method doesn't return anything but save all the outputs for the model.
+    Args:
+        xyi (Tuple[Tensor]): A tuple with (videos, labels, patient_ids, frames_ids, videos_ids) elements in that respective order.
+        fake_images (Tensor): A tensor with the generated videos by the model.
+        latent_i (Tensor): A tensor with the latent vector Zg of the model.
+        latent_o (Tensor): A tensor with the latent vector Z'g of the model.
+        feat_real (Tensor): A tensor with the latent vector Zd of the model.
+        feat_fake (Tensor): A tensor with the latent vector Z'd of the model.
+        outputs_path (List[Str]): A list with the paths in which the model stores elements such as latent vectors, errors and videos.
+        training (Boolean): Select True if the videos comes from the train data or False otherwise.
+        clean_old (Boolean): Select True if you want to delete all the content in outputs folder or False otherwise.
+    """
+    if clean_old:
+        for path in outputs_path:
+            os.system("rm -rf {}".format(os.path.join(path, "*")))
+
+    save_latent_vectors(tf.squeeze(latent_i).numpy(), xyi[1].numpy(), xyi[2].numpy(), xyi[3].numpy(), xyi[4].numpy(), outputs_path[0], training)
+    save_latent_vectors(tf.squeeze(latent_o).numpy(), xyi[1].numpy(), xyi[2].numpy(), xyi[3].numpy(), xyi[4].numpy(), outputs_path[1], training)
+    save_latent_vectors(tf.reshape(feat_real, [xyi[0].shape[0], -1]).numpy(), xyi[1].numpy(), xyi[2].numpy(), xyi[3].numpy(), xyi[4].numpy(), outputs_path[2], training)
+    save_latent_vectors(tf.reshape(feat_fake, [xyi[0].shape[0], -1]).numpy(), xyi[1].numpy(), xyi[2].numpy(), xyi[3].numpy(), xyi[4].numpy(), outputs_path[3], training)
+
+    batch_frames = np.r_[[min_max_scaler(i, 0, 0, 255, 0)[0].numpy() for i in xyi[0]]].astype(np.uint8)
+    save_frames(batch_frames, xyi[1].numpy(), xyi[2].numpy(), xyi[3].numpy(), xyi[4].numpy(), outputs_path[4], training)
+    batch_frames = np.r_[[min_max_scaler(i, 0, 0, 255, 0)[0].numpy() for i in fake_images]].astype(np.uint8)
+    save_frames(batch_frames, xyi[1].numpy(), xyi[2].numpy(), xyi[3].numpy(), xyi[4].numpy(), outputs_path[5], training)
+    batch_frames = np.r_[[min_max_scaler(i, 0, 0, 255, 0)[0].numpy() for i in tf.abs(xyi[0] - fake_images)]].astype(np.uint8)
+    save_frames(batch_frames, xyi[1].numpy(), xyi[2].numpy(), xyi[3].numpy(), xyi[4].numpy(), outputs_path[6], training)
+
+    save_errors(l2_loss_batch(feat_real, feat_fake).numpy(), xyi[1].numpy(), outputs_path[7], training)
+    save_errors(l1_loss_batch(xyi[0], fake_images).numpy(), xyi[1].numpy(), outputs_path[8], training)
+    save_errors(l2_loss_batch(latent_i, latent_o).numpy(), xyi[1].numpy(), outputs_path[9], training)
